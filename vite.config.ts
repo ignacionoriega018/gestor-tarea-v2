927024ace63de26303bf9feca806451686b7553d
ignacionoriega018 <ignacio.noriega@eset.com>
2024-11-01 03:44:12 +0100

    Update vite.config.ts new
    nrew

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gestor-tarev2/', // Añade esta línea
+  base: '/gestor-tarea-v2/', // Añade esta línea
 })
